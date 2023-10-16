4e175066d876cba2f53aa0f63a926dbebcfd28e0
7IronSnow7 <divan.debruin25@gmail.com>
2023-10-15 22:09:15 -0400

    Added ValidAnagram

---
+++ Algorithms/ValidAnagram.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class ValidAnagram
+{
+	public static bool Solution(string s, string t)
+	{
+		// Anagram will have the same length
+		if(s.Length != t.Length) return false;
+
+		// Convert to character Array
+		char[] charS = s.ToCharArray();
+		char[] charT = t.ToCharArray();
+
+		// Sort through the array ascending order
+		Array.Sort(charS);
+		Array.Sort(charT);
+
+		// Convert back to string
+		var sortedS = new string(charS);
+		var sortedT = new string(charT);
+
+		// Compare the values
+		return sortedS == sortedT;
+	}
+}
